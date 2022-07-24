4fc9e3b14a136852c90922951ac95488cedf02a0
juanestebanperezm <flievano100@misorobotics.com>
2022-07-23 19:25:50 -0500

    ee

--- src/main.py
+++ src/main.py
@@ -1,14 +1,9 @@
-import pyautogui as pg
-import selenium
 import time
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 
 
